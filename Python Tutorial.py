754485a0322e7418de08236007566c5d488dc0ba
AGithubUs <138012249+AGithubUs@users.noreply.github.com>
2023-10-11 20:42:47 +0800

    Add files via upload

---
+++ Python Tutorial.py
@@ -0,0 +1,28 @@
+from time import sleep
+x = input("Python Basic Tutorial Password Protected, Type Tutorial To Begin:  ")
+password = "Tutorial"
+
+if x == password:
+
+    print("You just Unlocked the password into this tutorial!")
+    sleep(4)
+    print("Loading...")
+    sleep(10)
+    print("This is an easy python tutorial")
+    sleep(10)
+    print("Hello, Welcome to this Python Tutorial, We will be learning about the print and sleep command today")
+    sleep(10)
+    print("print is a command used to display text in the terminal")
+    sleep(10)
+    print("You use print in cases of typing into a terminal or telling something to do something in Python code.")
+    sleep(10)
+    print("The Sleep command is different")
+    sleep(10)
+    print("The Sleep command holds a line of code for a certain time the user selects;")
+    sleep(10)
+    print("This whole code is made from sleep and print commands")
+    sleep(10)
+    print("The Sleep command holds code in seconds (60 = 1 Minute)")
+    sleep(10)
+    print("Thats All in this Easy Python Tutorial,")
+    sleep(90)
